a7356c20f07e1e2470a1ccf36d21da772e2f21e4
Deyan Nikolov <didonikolovbg@gmail.com>
2023-01-17 14:59:38 +0200

    Delete Confd.

--- website/api/apihandler.py
+++ website/api/apihandler.py
@@ -50,12 +50,11 @@ def worker(token, worker):
     print("worker: " + worker)
 
     if request.method == 'POST':
-        return jsonify({"status": "error", "message": "POST method is not allowed for this operation. See documentation for more information."}), 405
-
+        return jsonify({"status": "error",
+                        "message": "POST method is not allowed for this operation. See documentation for more information."}), 405
 
     if token == "not_defined_variable" or worker == "not_defined_variable":
         return jsonify({"error": "Missing required parameters"}), 400
-
 
     user_from_token = Worker.query.filter_by(token=token).first()
     if not user_from_token:
@@ -67,7 +66,6 @@ def worker(token, worker):
     if not worker:
         return jsonify({"error": "Worker not found"}), 404
 
-
     if user_from_token.accounttype == "worker":
         if worker.id != user_from_token.id:
             return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
@@ -75,95 +73,85 @@ def worker(token, worker):
         if worker.boss_id != user_from_token.id:
             return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
 
-
     workerdict = worker.to_dict()
 
     return jsonify({"status": "success", "worker": workerdict}), 200
 
+
 @api.route('/v1/employee/<token>/<worker>/tasks', methods=['GET', 'POST'])
 def worker_tasks(token, worker):
 
-        print("token: " + token)
-        print("worker: " + worker)
+    print("token: " + token)
+    print("worker: " + worker)
 
-        if request.method == 'POST':
-            return jsonify({"status": "error", "message": "POST method is not allowed for this operation. See documentation for more information."}), 405
+    if request.method == 'POST':
+        return jsonify({"status": "error",
+                        "message": "POST method is not allowed for this operation. See documentation for more information."}), 405
 
+    if token == "not_defined_variable" or worker == "not_defined_variable":
+        return jsonify({"error": "Missing required parameters"}), 400
 
-        if token == "not_defined_variable" or worker == "not_defined_variable":
-            return jsonify({"error": "Missing required parameters"}), 400
-
-
-        user_from_token = Worker.query.filter_by(token=token).first()
+    user_from_token = Worker.query.filter_by(token=token).first()
+    if not user_from_token:
+        user_from_token = Boss.query.filter_by(token=token).first()
         if not user_from_token:
-            user_from_token = Boss.query.filter_by(token=token).first()
-            if not user_from_token:
-                return jsonify({"error": "Authentication failed"}), 403
+            return jsonify({"error": "Authentication failed"}), 403
 
-        worker = Worker.query.filter_by(id=worker).first()
-        if not worker:
-            return jsonify({"error": "Worker not found"}), 404
+    worker = Worker.query.filter_by(id=worker).first()
+    if not worker:
+        return jsonify({"error": "Worker not found"}), 404
 
+    if user_from_token.accounttype == "worker":
+        if worker.id != user_from_token.id:
+            return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
 
-        if user_from_token.accounttype == "worker":
-            if worker.id != user_from_token.id:
-                return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
+    else:
+        if worker.boss_id != user_from_token.id:
+            return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
 
-        else:
-            if worker.boss_id != user_from_token.id:
-                return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
+    tasks = Task.query.filter_by(worker_id=worker.id).all()
+    tasksdict = {}
+    for task in tasks:
+        tasksdict[str(task.id)] = task.to_dict()
 
-        tasks = Task.query.filter_by(worker_id=worker.id).all()
-        tasksdict = {}
-        for task in tasks:
-            tasksdict[str(task.id)] = task.to_dict()
-
-        return jsonify({"status": "success", "tasks": tasksdict}), 200
+    return jsonify({"status": "success", "tasks": tasksdict}), 200
 
 
 @api.route('/v1/employee/<token>/<worker>/tasks/<task>', methods=['GET', 'POST'])
 def worker_task(token, worker, task):
 
-        print("token: " + token)
-        print("worker: " + worker)
-        print("task: " + task)
+    if request.method == 'POST':
+        return jsonify({"status": "error",
+                        "message": "POST method is not allowed for this operation. See documentation for more information."}), 405
 
-        if request.method == 'POST':
-            return jsonify({"status": "error", "message": "POST method is not allowed for this operation. See documentation for more information."}), 405
+    if token == "not_defined_variable" or worker == "not_defined_variable" or task == "not_defined_variable":
+        return jsonify({"error": "Missing required parameters"}), 400
 
-
-        if token == "not_defined_variable" or worker == "not_defined_variable" or task == "not_defined_variable":
-            return jsonify({"error": "Missing required parameters"}), 400
-
-
-        user_from_token = Worker.query.filter_by(token=token).first()
+    user_from_token = Worker.query.filter_by(token=token).first()
+    if not user_from_token:
+        user_from_token = Boss.query.filter_by(token=token).first()
         if not user_from_token:
-            user_from_token = Boss.query.filter_by(token=token).first()
-            if not user_from_token:
-                return jsonify({"error": "Authentication failed"}), 403
+            return jsonify({"error": "Authentication failed"}), 403
 
-        worker = Worker.query.filter_by(id=worker).first()
-        if not worker:
-            return jsonify({"error": "Worker not found"}), 404
+    worker = Worker.query.filter_by(id=worker).first()
+    if not worker:
+        return jsonify({"error": "Worker not found"}), 404
 
+    if user_from_token.accounttype == "worker":
+        if worker.id != user_from_token.id:
+            return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
 
-        if user_from_token.accounttype == "worker":
-            if worker.id != user_from_token.id:
-                return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
+    else:
+        if worker.boss_id != user_from_token.id:
+            return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
 
-        else:
-            if worker.boss_id != user_from_token.id:
-                return jsonify({"error": "Your current token is not allowed to access this worker"}), 403
+    task = Task.query.filter_by(id=task).first()
+    if not task:
+        return jsonify({"error": "Task not found"}), 404
 
-        task = Task.query.filter_by(id=task).first()
-        if not task:
-            return jsonify({"error": "Task not found"}), 404
+    if task.worker_id != worker.id:
+        return jsonify({"error": "Your current token is not allowed to access this task"}), 403
 
-        if task.worker_id != worker.id:
-            return jsonify({"error": "Your current token is not allowed to access this task"}), 403
+    taskdict = task.to_dict()
 
-        taskdict = task.to_dict()
-
-        return jsonify({"status": "success", "task": taskdict}), 200
-
-
+    return jsonify({"status": "success", "task": taskdict}), 200
