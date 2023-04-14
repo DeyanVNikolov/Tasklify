c50b23d95414b10abe9d2685e209b8b3657ae5bb
Deyan Nikolov <didonikolovbg@gmail.com>
2023-04-15 00:48:32 +0300

    Security Notice

--- website/static/themes.js
+++ website/static/themes.js
@@ -9,3 +9,34 @@ function lighttheme() {
     window.location.reload();
 }
 
+
+function printWarningRed(message) {
+  const bigMessage = message.toUpperCase();
+  console.log(`%c\n%s\n`, `font-size: 30px; color: red; font-weight: bold;`, bigMessage);
+}
+
+function printWarningRedSmall(message) {
+  const bigMessage = message.toUpperCase();
+  console.log(`%c\n%s\n`, `font-size: 20px; color: red; font-weight: bold;`, bigMessage);
+}
+
+function printWarningYellow(message) {
+    const bigMessage = message.toUpperCase();
+    console.log(`%c\n%s\n`, `font-size: 20px; color: yellow; font-weight: bold;`, bigMessage);
+}
+
+function printWarningYellowSmall(message) {
+    const bigMessage = message.toUpperCase();
+    console.log(`%c\n%s\n`, `font-size: 15px; color: yellow; font-weight: bold;`, bigMessage);
+}
+
+console.error("SECURITY NOTICE");
+printWarningRed("!!! WARNING !!!");
+printWarningYellow("This is a security notice!");
+printWarningYellow("Unless you know what you are doing, do not proceed!");
+printWarningYellow("Pasting anything into the console can compromise your account and data!");
+printWarningYellowSmall("Tasklify is not responsible for any damage caused by pasting anything into the console!");
+printWarningRedSmall("Continue at your own risk!");
+printWarningRed("!!! WARNING !!!");
+console.error("SECURITY NOTICE");
+console.error("Your data is at risk! Do not paste anything into the console unless you know what you are doing!")
